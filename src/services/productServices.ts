be7595ad77cc195fd7949e8294ea48cd9fd1a20c
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-12-08 10:05:30 +0200

    add product services

---
+++ src/services/productServices.ts
@@ -0,0 +1,15 @@
+import { Product } from "../model/product";
+import { Axios } from "../util/axios";
+import { PageInput } from "../util/page";
+
+export class ProductServices{
+    public async createProduct(product:Product){
+        return Axios().post(`/product/create`,product); 
+    }
+    public async productList(page:PageInput){
+        return Axios().post(`/product/find-all`,page); 
+    }
+    public async deleteProduct(id:PageInput){
+        return Axios().delete(`/product/delete-product/${id}`); 
+    }
+}
