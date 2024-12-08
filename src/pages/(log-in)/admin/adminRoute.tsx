de6fc03c4ea13140f865f56c910b51cf9f4089a8
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-12-08 10:22:44 +0200

    add new routes

--- src/pages/(log-in)/admin/adminRoute.tsx
+++ src/pages/(log-in)/admin/adminRoute.tsx
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router-dom"
 import { AdminHomePage } from "."
 import { AdminManageUserUI } from "./users"
 import { AdminStockManagementUI } from "./stock"
+import { AdminManageIncidentUI } from "./incidence"
+import { AdminManageMiningUI } from "./mining"
 
 export const AdminRoutes = () => {
     return <>
@@ -9,6 +11,11 @@ export const AdminRoutes = () => {
             <Route index element={<AdminHomePage />} />
             <Route path="users" element={<AdminManageUserUI />} />
             <Route path="stock" element={<AdminStockManagementUI />} />
+            <Route path="incidence" element={<AdminManageIncidentUI />} />
+            <Route path="mining" element={<AdminManageMiningUI />} />
+            <Route path="mining" element={<AdminManageMiningUI />} />
+            <Route path="mining" element={<AdminManageMiningUI />} />
+            
         </Routes>
     </>
 }
