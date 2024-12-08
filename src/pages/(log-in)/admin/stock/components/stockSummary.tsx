e06ca406f79430b85f5b7e3856a48c1d5b71501c
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-12-08 10:23:30 +0200

    change style

--- src/pages/(log-in)/admin/stock/components/stockSummary.tsx
+++ src/pages/(log-in)/admin/stock/components/stockSummary.tsx
@@ -2,7 +2,7 @@ import { BarChart } from "@mui/icons-material"
 import { Card } from "@mui/material"
 
 export const StockSummary = () => {
-  return <div className="md:flex gap-4">
+  return <div className="grid grid-cols-4 gap-4">
   {[...new Array(4)].map(data=><Card key={data} className="flex items-center justify-between p-4 gap-2">
     <BarChart/>
     <div>
